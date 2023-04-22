3d984f976533f3c6e659a59a2bad84abe29f7aee
Daniel Sunny <danisun2510@gmail.com>
2023-04-22 18:46:31 +0530

    Creating ticket and viewing ticket functionality added

--- src/main/java/com/ticket/tracker/controller/homeController.java
+++ src/main/java/com/ticket/tracker/controller/homeController.java
@@ -1,5 +1,59 @@
 package com.ticket.tracker.controller;
 
-public class homeController {
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ticket.tracker.entity.Tickets;
+import com.ticket.tracker.repository.TicketRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class homeController {
+	
+	@Autowired
+	private TicketRepository ticketRepo;
+	
+	@GetMapping("/")
+	public String home(Model m)
+	{
+		List<Tickets> list = ticketRepo.findAll();
+		if(list == null)
+		{
+			System.out.println("asdasdasdasdasdasd");
+		}
+		System.out.println("Hello");
+		System.out.println(list);
+		m.addAttribute("tickets", list);
+		
+		return "home";
+	}
+	
+	@GetMapping("/create")
+	public String create_ticket()
+	{
+		return "createTicket";
+	}
+	
+	@GetMapping("/delete")
+	public void delete_ticket()
+	{
+		return ;
+	}
+	
+	@PostMapping("/make_ticket")
+	public String add_ticket(@ModelAttribute Tickets tkt_details, HttpSession session) {
+		
+		tkt_details.setDate(LocalDate.now());
+		ticketRepo.save(tkt_details);
+		session.setAttribute("msg", "Added successfully");
+		return "redirect:/create";
+	}
 }
--- src/main/java/com/ticket/tracker/entity/Tickets.java
+++ src/main/java/com/ticket/tracker/entity/Tickets.java
@@ -2,7 +2,6 @@ package com.ticket.tracker.entity;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,6 +33,10 @@ public class Tickets {
 		this.date = date;
 	}
 
+	public long getId() {
+		return this.id;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
